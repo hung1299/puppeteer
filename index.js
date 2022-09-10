9b96fadf636cdb4a2bcdf7f1709415946d37b060
hung1299 <dotaallstar1999@gmail.com>
2022-09-10 12:39:08 +0700

    minor update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const puppeteer = require("puppeteer");
 const path = require("path");
 const NETWORK_STATUS = "networkidle2";
+const TEXT_TYPE = "text";
+const IMAGE_TYPE = "image";
+const LINK_TYPE = "link";
 
 const users = [
     {
@@ -10,34 +13,73 @@ const users = [
         blogs: [
             {
                 title: "Up Up and Away!",
-                content:
-                    "Takes 12 steps, but I hit a brick wall at 8. Took time off to recuperate and recover my energy. Now back from paradise, rejuvenated and ready to take on the beast again. All eyes on the prize, one more trip to deliver the coup de grace to that other beast in the west. Once that's done, its just a matter of time and protocol until the next step is reached. Its scary and new, but isn't that just what makes it all so deliciously exciting?",
-                image: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1520x800.png",
+                content: [
+                    {
+                        type: TEXT_TYPE,
+                        data: "Takes 12 steps, but I hit a brick wall at 8. Took time off to recuperate and recover my energy. Now back from paradise, rejuvenated and ready to take on the beast again. All eyes on the prize, one more trip to deliver the coup de grace to that other beast in the west. Once that's done, its just a matter of time and protocol until the next step is reached. Its scary and new, but isn't that just what makes it all so deliciously exciting?",
+                    },
+                    {
+                        type: IMAGE_TYPE,
+                        data: "https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1520x800.png",
+                    },
+                    {
+                        type: LINK_TYPE,
+                        text: "google.com",
+                        data: "https://www.google.com/search?q=edit+value+of+text+editor+js&sxsrf=ALiCzsbDcJo_1KeQNargcFXgggVHIhUXTg%3A1662778718228&ei=Xv0bY8jEDcjT2roPqu2YuAo&ved=0ahUKEwjI69u2nYn6AhXIqVYBHao2BqcQ4dUDCA8&uact=5&oq=edit+value+of+text+editor+js&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKABMgUIIRCgATIFCCEQoAEyBQghEKABMggIIRAeEBYQHTIICCEQHhAWEB0yCAghEB4QFhAdMggIIRAeEBYQHTIICCEQHhAWEB0yCAghEB4QFhAdOgoIABBHENYEELADOgoIIRAeEA8QFhAdSgUIPBIBMUoECEEYAEoECEYYAFDOBFj1B2DuCGgBcAF4AIABeYgB8QGSAQMwLjKYAQCgAQHIAQjAAQE&sclient=gws-wiz#bsht=CgRmYnNtEgYIBBAAGBE",
+                    },
+                ],
             },
-            {
-                title: "Do Nots",
-                content:
-                    "Spurred on by my observations at work, I have come up with a list of things to avoid as a doctor in a hospital setting (especially for interns and medical students):",
-                image: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
-            },
-            {
-                title: "Brighter Days Ahead",
-                content:
-                    "Resolution of conflicts on many fronts helps, but more is to come. Once we've jetted off east things will really start looking up. But until then: paint, parking, wood, tables, chairs, sardar, food, drink, lists, healthy doses of work and many more..",
-                image: "https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300",
-            },
-            {
-                title: "Eureka!",
-                content:
-                    "I've always had a thing against Landon Donovan. The tag of 'best US soccer player of his generation' rang a bit hollow when you looked at what he's achieved on a global scale. His two aborted attempts at Bayer Leverkusen in Germany really frustrated me.",
-                image: "https://www.w3schools.com/w3css/img_forest.jpg",
-            },
-            {
-                title: "Good Point",
-                content:
-                    "Having a terrible morning all round today. Overworked and undervalued, I was sitting in the clinic and I locked myself in the room for half an hour to clear my thoughts before seeing any patients.",
-                image: "https://h5p.org/sites/default/files/h5p/content/1209180/images/file-6113d5f8845dc.jpeg",
-            },
+            // {
+            //     title: "Do Nots",
+            //     content: [
+            //         {
+            //             type: TEXT_TYPE,
+            //             data: "Spurred on by my observations at work, I have come up with a list of things to avoid as a doctor in a hospital setting (especially for interns and medical students):",
+            //         },
+            //         {
+            //             type: IMAGE_TYPE,
+            //             data: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
+            //         },
+            //         {
+            //             type: LINK_TYPE,
+            //             data: "https://www.google.com/search?q=edit+value+of+text+editor+js&sxsrf=ALiCzsbDcJo_1KeQNargcFXgggVHIhUXTg%3A1662778718228&ei=Xv0bY8jEDcjT2roPqu2YuAo&ved=0ahUKEwjI69u2nYn6AhXIqVYBHao2BqcQ4dUDCA8&uact=5&oq=edit+value+of+text+editor+js&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKABMgUIIRCgATIFCCEQoAEyBQghEKABMggIIRAeEBYQHTIICCEQHhAWEB0yCAghEB4QFhAdMggIIRAeEBYQHTIICCEQHhAWEB0yCAghEB4QFhAdOgoIABBHENYEELADOgoIIRAeEA8QFhAdSgUIPBIBMUoECEEYAEoECEYYAFDOBFj1B2DuCGgBcAF4AIABeYgB8QGSAQMwLjKYAQCgAQHIAQjAAQE&sclient=gws-wiz#bsht=CgRmYnNtEgYIBBAAGBE",
+            //         },
+            //     ],
+            // },
+            // {
+            //     title: "Brighter Days Ahead",
+            //     content: [
+            //         {
+            //             type: TEXT_TYPE,
+            //             data: "Resolution of conflicts on many fronts helps, but more is to come. Once we've jetted off east things will really start looking up. But until then: paint, parking, wood, tables, chairs, sardar, food, drink, lists, healthy doses of work and many more..",
+            //         },
+            //         {
+            //             type: IMAGE_TYPE,
+            //             data: "https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300",
+            //         },
+            //         {
+            //             type: LINK_TYPE,
+            //             data: "https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300",
+            //         },
+            //     ],
+            // },
+            // {
+            //     title: "Eureka!",
+            //     content: [
+            //         {
+            //             type: TEXT_TYPE,
+            //             data: "I've always had a thing against Landon Donovan. The tag of 'best US soccer player of his generation' rang a bit hollow when you looked at what he's achieved on a global scale. His two aborted attempts at Bayer Leverkusen in Germany really frustrated me.",
+            //         },
+            //         {
+            //             type: IMAGE_TYPE,
+            //             data: "https://www.w3schools.com/w3css/img_forest.jpg",
+            //         },
+            //         {
+            //             type: LINK_TYPE,
+            //             data: "https://www.w3schools.com/w3css/img_forest.jpg",
+            //         },
+            //     ],
+            // },
         ],
     },
 ];
@@ -84,6 +126,24 @@ const goToCreatePost = async (page, baseURL) => {
     });
 };
 
+const handleFillContent = async (content, page) => {
+    switch (content.type) {
+        case TEXT_TYPE:
+            await page.keyboard.type(content.data);
+            await page.keyboard.press("Enter");
+            break;
+        case IMAGE_TYPE:
+            await fillImage(content.data, page);
+            break;
+        case LINK_TYPE:
+            await page.keyboard.type(content.data);
+            await page.keyboard.press("Enter");
+            break;
+        default:
+            break;
+    }
+};
+
 const fillTitleAndContent = async (page, blog) => {
     await page
         .waitForSelector("textarea[data-hook='post-form__title-input']")
@@ -92,8 +152,9 @@ const fillTitleAndContent = async (page, blog) => {
             await page.keyboard.type(blog.title);
         });
     await page.click("[data-hook='post-form__content']");
-    await page.keyboard.type(blog.content);
-    await fillImage(blog.image, page);
+    for (const data of blog.content) {
+        await handleFillContent(data, page);
+    }
     const submitBtn = await page.waitForSelector(
         "[data-hook='topbar-publish-button'][aria-disabled='false']",
         { timeout: 200000 }
@@ -145,9 +206,11 @@ const fillImage = async (url, page) => {
     await page.keyboard.press("ArrowDown");
 };
 
+const fillLink = async(link);
+
 const auto = async (user) => {
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         args: [
             `--user-data-dir=${path.resolve("./user-data/" + user.name)}`,
             "--disable-web-security",
@@ -156,7 +219,6 @@ const auto = async (user) => {
         ],
         timeout: 100000,
     });
-    console.log("get test");
     let page = await browser.newPage();
     page.on("dialog", async (dialog) => {
         await dialog.accept();
@@ -189,7 +251,7 @@ const auto = async (user) => {
             console.log("blog ", index);
         } catch (error) {
             console.log("something wrong with blog: ", blog);
-            console.log("error: ", error.message);
+            console.log("error: ", error);
         }
     }
     await goToBlogPosts(page, baseURL);
